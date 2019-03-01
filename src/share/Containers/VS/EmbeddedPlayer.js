9ce01fc24b27ca041a101cd9e36e523191d2fdd1
alex nikonov <alexmotorniyg@gmail.com>
2019-03-01 16:00:42 +0100

    embedded player animation is fixed

--- src/share/Containers/VS/EmbeddedPlayer.js
+++ src/share/Containers/VS/EmbeddedPlayer.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export const EmbeddingPlayer = ({ youtubeID }) => (
-	<div className='EmbeddingPlayer'>
-			{youtubeID ? (<iframe src={`https://www.youtube.com/embed/${youtubeID}?ecver=1&amp;iv_load_policy=3&amp;rel=0&amp;showinfo=0&amp;yt:stretch=16:9&amp;autohide=1&amp;color=red&amp;width=560&amp;width=560`} width="560" height="315" allowtransparency="true" frameBorder="0"/>) : 'no content available'}
-	</div>
-)
+import animationTypes from '../../../assets/styles/styles.scss';
+
+class EmbeddingPlayer extends Component {
+	state = {
+		// isChanging: true
+		className: ''
+	}
+
+	// previewChangeAction = (isChanging) => {
+	// 	console.log('ischanging bool getting changed')
+	// 	this.setState({ isChanging });
+	// }
+
+	applyClassName = (isChanging) => {
+		console.log('hi every time from changing func')
+		const classArray = ['EmbeddingPlayer'];
+		let className;
+		if (isChanging) {
+			classArray.push('changing');
+			className = classArray.join(" ");
+		}
+		if (!isChanging){ className = classArray.join(" ") };
+		console.log('classname is applied', className);
+		return this.setState({className})
+	}
+
+	componentDidUpdate = (prevProps) => {
+		console.log('compoent did update with', prevProps.youtubeID);
+	}
+
+	componentWillReceiveProps = (nextProps) => {
+		const nextItem = nextProps.youtubeID;
+		const prevItem = this.props.youtubeID;
+		if (nextItem !== prevItem) {
+			// this.previewChangeAction(true);
+			console.log('it gets props twice')
+			this.applyClassName(true)
+		}
+	}
+
+	render() {
+		const { youtubeID } = this.props;
+		// let className = this.applyClassName(this.state.isChanging);
+		return (
+			<div className={this.state.className}
+				onAnimationEnd={
+					({ animationName }) => {
+						console.log(animationName, 'animationName and waited name', animationTypes.flashInEffect, 'hello from animation end');
+						if(animationName === animationTypes.flashInEffect) {
+							this.applyClassName(false)
+					}}}
+			>
+				{youtubeID ? (<iframe src={`https://www.youtube.com/embed/${youtubeID}?ecver=1&amp;iv_load_policy=3&amp;rel=0&amp;showinfo=0&amp;yt:stretch=16:9&amp;autohide=1&amp;color=red&amp;width=560&amp;width=560`} width="560" height="315" allowtransparency="true" frameBorder="0" />) : 'no content available'}
+			</div>
+		)
+	}
+}
 
 const mapStateToProps = state => ({ previewList: state.VScontent.previewList });
 
--- src/share/Containers/VS/PreviewThumb.js
+++ src/share/Containers/VS/PreviewThumb.js
@@ -4,22 +4,21 @@ import PropTypes from "prop-types";
 
 import Button from '../../Containers/UI/Button';
 import * as actionCreator from '../../../store/actions/index'
-import animationTypes from '../../../assets/styles/styles.scss';
 
 class PreviewThumb extends Component {
-	state = { 
-		isChanging: true
-	}
+	// state = { 
+	// 	isChanging: true
+	// }
 
-	previewChangeAction = (isChanging) => this.setState({ isChanging });
+	// previewChangeAction = (isChanging) => this.setState({ isChanging });
 	
-	componentWillReceiveProps = (nextProps) => {
-		const nextItem = nextProps.previewList[nextProps.i];
-		const prevItem = this.props.previewList[this.props.i];
-		if (nextItem != prevItem) {
-			this.previewChangeAction(true);
-		}
-	}
+	// componentWillReceiveProps = (nextProps) => {
+	// 	const nextItem = nextProps.previewList[nextProps.i];
+	// 	const prevItem = this.props.previewList[this.props.i];
+	// 	if (nextItem != prevItem) {
+	// 		this.previewChangeAction(true);
+	// 	}
+	// }
 
 	render() {
 		const {
@@ -30,22 +29,17 @@ class PreviewThumb extends Component {
 			getClickedIndexAction,
 			openDragWrapperAction
 		} = this.props;
-		const { isChanging }= this.state;
+		// const { isChanging }= this.state;
 		return (
 			<div className='PreviewThumb' >
 				<h6 className='title'>{previewList[i].title}</h6>
 				<img
-					className={isChanging ? 'changing' : null}
 					src={`https://img.youtube.com/vi/${previewList[i].youtubeID}/0.jpg`}
 					alt={previewList[i].title}
 					onClick={() => {
 						getClickedIndexAction(i);
 						openDragWrapperAction()
 					}}
-					onAnimationEnd={
-						({ animationName }) => {
-							animationName === animationTypes.flashEffect && this.previewChangeAction(false)
-						}}
 				/>
 				<div className="footer">
 					<Button btntype={cancelBtn}>Not interesting</Button>
